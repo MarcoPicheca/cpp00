a1a286b728a35b4f950fb35df88e6fe82df71495
Marco Picheca <marco.picheca@gmail.com>
2025-01-14 16:06:20 +0100

    megaphone done; phonebook halfway

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -1,4 +1,42 @@
-#include <iostream>
+#include "./PhoneBook.hpp"
 
 
 int main()
+{
+	PhoneBook phonebook;
+	std::string command;
+
+	int i = 0;
+	// phonebook.c_arr[i].get_contact();
+	while (1)
+	{
+		if (i == 8)
+			i = 0;
+		std::cout << "Write a command between ADD, SEARCH and EXIT (UPPERCASE REQUIRED): ";
+		std::cin >> command;
+
+		if (command.compare("ADD") == 0)
+		{
+			std::cout << "Write name: ";
+			std::cin >> phonebook.tmp_name;
+			std::cout << "Write phone number: ";
+			std::cin >> phonebook.tmp_num;
+			std::cout << "Write last name: ";
+			std::cin >> phonebook.tmp_lst;
+			std::cout << "Write nickname: ";
+			std::cin >> phonebook.tmp_nick;
+			std::cout << "Write his/her darkest secret: ";
+			std::cin >> phonebook.tmp_secret;
+			phonebook.c_arr[i].add_cont(phonebook.tmp_name, phonebook.tmp_num,
+				phonebook.tmp_lst, phonebook.tmp_nick, phonebook.tmp_secret, i);
+			i++;
+		}
+		// else if (command.compare("SEARCH") == 0)
+		// 	// add contact
+		// 	;
+		else if (command.compare("EXIT") == 0)
+			exit(0);
+		else
+			std::cout << "READ AGAIN" << std::endl;
+	}
+}
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,24 +6,65 @@
 /*   By: mapichec <mapichec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 20:05:22 by mapichec          #+#    #+#             */
-/*   Updated: 2025/01/11 20:06:08 by mapichec         ###   ########.fr       */
+/*   Updated: 2025/01/12 18:42:21 by mapichec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONE_BOOK_H
 # define PHONE_BOOK_H
+# include <iostream>
+# include <cstring>
 
 class Contact
 {
 	public:
-		int	cont_num;
+		Contact(){std::memset(this, 0, sizeof(*this));}
+	// funzioni per modificare da phonebook
+		void add_cont(std::string nm, std::string num,
+			std::string lst_nm, std::string nk_nm, std::string scrt, 
+			int num_cont)
+		{
+			this->name.replace(0, nm.length(), nm);
+			this->number.replace(0, num.length(), num);
+			this->last_name.replace(0, lst_nm.length(), lst_nm);
+			this->nickname.replace(0, nk_nm.length(), nk_nm);
+			this->secret.replace(0, scrt.length(), scrt);
+			this->cont_old = num_cont;
+		};
+		
+		//TODO funzione per il search con regole per la lista settata
+	private:
+		std::string name;
+		std::string nickname;
+		std::string last_name;
+		std::string number;
+		std::string secret;
 		int cont_old;
+	/** 
+	 * utile per visual
+	 * void get_contact()
+	 * {
+	 * 	std::cout << this->name << std::endl;
+	 * 	std::cout << this->number << std::endl;
+	 * 	std::cout << this->last_name << std::endl;
+	 * 	std::cout << this->nickname << std::endl;
+	 * 	std::cout << this->secret << std::endl;
+	 * 	std::cout << this->cont_old << std::endl;
+	 * }
+	 * 
+	 * */ 
 };
 
 class PhoneBook
 {
 	public:
-		Contact arr[8];
+		Contact c_arr[8];
+		std::string tmp_name;
+		std::string tmp_num;
+		std::string tmp_lst;
+		std::string tmp_nick;
+		std::string tmp_secret;
+		// funzioni per modificare dal main
 };
 
 #endif
